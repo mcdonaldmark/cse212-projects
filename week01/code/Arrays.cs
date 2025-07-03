a5f4ab5800c7b6cac8a17e9768c645b9c2f4cbd6
mcdonaldmark <markamcdonald7@gmail.com>
2025-07-03 11:58:43 -0600

    sync

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -6,14 +6,18 @@ public static class Arrays
     /// integer greater than 0.
     /// </summary>
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
+  
+    /// Step 1: Pass on the double array "results" to be an array that goes by "length" for the size of the multiples. 
+    /// Step 2: Create the condition with a starting value of 1 that increments until it reaches the array length (length being specified by the user)
+    /// Step 3: With the iterator set at 1, I cannot use the results[1] as that will indicate the incorrect position so I needed to do result[i - 1] so that the array starts at index 0.
+    /// Step 4: Set the results[i - 1] (index) to equal the number used for the multiple and multiply it by the iterator (i)
+    /// Step 5: Return the result of the multiple array.
     public static double[] MultiplesOf(double number, int length)
     {
-        //return type of the method is array of double data type, so we are declaring the return variable with double[] datatype.
         double[] results = new double[length];
 
         for (int i = 1; i <= length; i++)
         {
-            //instead of mod operation, you need to multiply the number and add to array.
             results[i - 1] = (number * i);
         }
         return results;
@@ -26,20 +30,20 @@ public static class Arrays
     ///
     /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
     /// </summary>
+
+    /// Step 1: First I will do a split where we count the data that's inside the list and then subtract how far we move the data to the right. 
+    /// Step 2: Get the range of the split (index) portion that is on the right of the data being split. This will create a new list with only that data that is on the right.
+    /// Step 3: Get the range of the remaining portion by starting at index 0 up to the split location. This will create a new list with only that data that is on the left.
+    /// Step 4: From there, clear the original list, add the right range first (since we are rotating to the right), and then add the left range. 
     public static void RotateListRight(List<int> data, int amount)
     {
-        // Step 2: Calculate the split point for the rotation.
-        int splitIndex = data.Count - amount;
+        int split = data.Count - amount;
 
-        // Step 3: Use GetRange to extract the two parts:
-        List<int> rightPart = data.GetRange(splitIndex, amount); // last 'amount' elements
-        List<int> leftPart = data.GetRange(0, splitIndex);       // the rest
+        List<int> right = data.GetRange(split, amount);
+        List<int> left = data.GetRange(0, split);
 
-        // Step 4: Clear the original list and add the two parts back in rotated order.
         data.Clear();
-        data.AddRange(rightPart);
-        data.AddRange(leftPart);
-
-        // Step 5: The original list is now rotated in-place.
+        data.AddRange(right);
+        data.AddRange(left);
     }
 }
